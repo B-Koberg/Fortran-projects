5c141d90ce6bef8279d5a4c6d4cd50993891469b
Bastian <b.koberg412@gmail.com>
2026-06-15 16:58:27 +0200

    Nice picture, relativetly goo proportion of max iter and resolution, txt too large and inefficient py

--- mandelbrot_calc/parameters.f90
+++ mandelbrot_calc/parameters.f90
@@ -2,8 +2,8 @@ module parameters
     implicit none
     public :: nx, ny, max_iter, x_min, x_max, y_min, y_max
 
-    integer, parameter :: nx = 1500, ny = 900
-    integer, parameter :: max_iter = 50000
+    integer, parameter :: nx = 2*7680, ny = 2*4320
+    integer, parameter :: max_iter = 100
     real(8), parameter :: x_min = -2.0d0, x_max = 1.0d0
     real(8), parameter :: y_min = -1.5d0, y_max = 1.5d0
 
